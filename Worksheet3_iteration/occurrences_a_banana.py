9f8491265e82363b812a216137c4886b366b04ea
Your Name <Kadeem.daniels3tech@gmail.com>
2025-09-07 15:23:53 -0600

    In the wise words of Gwen Stefani: this 💩 is bananas!

---
+++ Worksheet3_iteration/occurrences_a_banana.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use a while loop to count the number of occurrences of the letter
+ 'a' in a string "banana".
+"""
+
+Ustring = "banana"
+counter = 0
+total = 9
+occurrences = 0
+#set up the while loop
+while "a" in Ustring:
+    counter = counter + 1
+    current_number= counter
+    occurrences = occurrences + current_number
+print(f"{occurrences}")
+
+
+
+
+"""
+I need to figure of how to count the number of "a's" in banana,as is the code 
+is running and outputting the number of a's, perhaps i need to look into some
+new functions.
+
+"""
