d8990a78d0570e5e1f256b15e8a47455777aee86
Your Name <you@example.com>
2025-09-06 20:23:49 -0600

    counting down from 10 to 1

---
+++ Worksheet3_iteration/print-10-1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use a while loop to print numbers from 10 down to 1.
+"""
+
+#step 1   
+counter_numbers = 11
+stop = 1
+    
+while counter_numbers > stop:
+     counter_numbers = counter_numbers - 1
+     print(f"{counter_numbers}")   
